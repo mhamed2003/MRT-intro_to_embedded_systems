cc208fa6c6f1d82b13890895263fb7bfd200361c
mohamed hassan <116904351+mhamed2003@users.noreply.github.com>
2023-10-22 19:54:32 +0300

    Delete task3/cod3.c

--- task3/cod3.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    int num1;
-    float num2;
-     printf("the size of int is %d bytes \n",sizeof(num1));
-     printf("the size of float is %d bytes",sizeof(num2));
-
-}
