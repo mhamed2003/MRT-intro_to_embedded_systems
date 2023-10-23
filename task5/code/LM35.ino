09afc4e22d34b92d496906a150860528090cacd1
mohamed hassan <momohsn2003@gmail.com>
2023-10-23 23:09:32 +0300

    add task5

---
+++ task5/code/LM35.ino
@@ -0,0 +1,18 @@
+const int lm35_pin = A0;	
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int temp_adc_val;
+  float temp_val;
+  temp_adc_val = analogRead(lm35_pin);	
+  temp_val = (temp_adc_val * 4.88);
+  temp_val = (temp_val/10);
+  Serial.print("Temperature = ");
+  Serial.print(temp_val);
+  Serial.print(" Degree Celsius\n");
+  delay(10000);
+
+}
